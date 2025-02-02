bc5b3e4dbf97faacb9a0df2bfc9f27d442a50f67
Kaloyan Yovchev <kyovchev@gmail.com>
2025-02-02 18:23:55 +0200

    Add support for recordings start time

--- board-recorder-react-webui/src/components/Recording/Recording.jsx
+++ board-recorder-react-webui/src/components/Recording/Recording.jsx
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 
 import RecordingEvents from "./RecordingEvents";
 
+import { formatUnixTimestamp } from "../../utils/formatters";
+
 export default function Recording() {
   const recording = useSelector((state) => state.recordings.data);
 
@@ -10,6 +12,8 @@ export default function Recording() {
       {recording && (
         <div>
           <p>Recording id: {recording.id}</p>
+          <p>Recording time: {formatUnixTimestamp(recording.start_time)}</p>
+
           <p>Recording name: {recording.name}</p>
           <p>Recording status: {recording.status}</p>
           {recording.status != "Not Found" && (
--- board-recorder-react-webui/src/utils/formatters.js
+++ board-recorder-react-webui/src/utils/formatters.js
@@ -1,3 +1,7 @@
 export function formatTime(time) {
   return `${Math.round(time)} s`;
 }
+
+export function formatUnixTimestamp(timestamp) {
+  return new Date(timestamp * 1000).toLocaleString();
+}
