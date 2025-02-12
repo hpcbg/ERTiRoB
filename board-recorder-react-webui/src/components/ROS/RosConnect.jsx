920cef5a525cf7662b6f5328062487c8b22a1061
Kaloyan Yovchev <kyovchev@gmail.com>
2025-02-12 10:58:54 +0200

    Add ErrorBlock for ROS connection errors

--- board-recorder-react-webui/src/components/ROS/RosConnect.jsx
+++ board-recorder-react-webui/src/components/ROS/RosConnect.jsx
@@ -1,29 +1,45 @@
 import { getRosBridgeAddress } from "../../query_utils/auth";
 
+import ErrorBlock from "../UI/ErrorBlock";
 import Button from "../UI/Button";
 import ContentSection from "../UI/ContentSection";
 
 export default function RosConnect({ reconnect, rosError }) {
   return (
-    <ContentSection header="ROS 2 Connection">
-      <p>
-        Websocket is set to wss://{getRosBridgeAddress()}{" "}
-        <Button type="link" style="button" to="/intro">
-          Edit
-        </Button>
-      </p>
-      <p>
-        <Button type="button" style="button" onClick={reconnect}>
-          Reconnect
-        </Button>
-      </p>
-      {rosError && (
+    <>
+      <ContentSection header="ROS 2 Connection">
         <p>
-          {rosError} If the ROS bridge server is using a self-signed certificate
-          you need to accept it by opening https://{getRosBridgeAddress()} in
-          your browser to add it as an exception.
+          Websocket is set to wss://{getRosBridgeAddress()}{" "}
+          <Button type="link" style="button" to="/intro">
+            Edit
+          </Button>
         </p>
+        <p>
+          <Button type="button" style="button" onClick={reconnect}>
+            Reconnect
+          </Button>
+        </p>
+      </ContentSection>
+      {rosError && (
+        <ErrorBlock
+          title="Cannot connect to ROS"
+          message={
+            <>
+              If rosbridge is using a self-signed certificate
+              <br />
+              you need to add it as an exception in your browser
+              <br />
+              by loading{" "}
+              <a href={`https://${getRosBridgeAddress()}`} target="_blank">
+                https://
+                {getRosBridgeAddress()}
+              </a>
+              <br />
+              and then reconnect to ROS.
+            </>
+          }
+        ></ErrorBlock>
       )}
-    </ContentSection>
+    </>
   );
 }
